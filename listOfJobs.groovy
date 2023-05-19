b69c858965736a535666ec51dc9b69de478000ee
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-19 17:54:56 +0700

    Update listOfJobs.groovy

--- listOfJobs.groovy
+++ listOfJobs.groovy
@@ -1,12 +1,12 @@
 jobsToTrigger = [
-    [job: 'test-1', params: [string(name: 'city', value: 'bogor'), string(name: 'province', value: 'west java')]],
-    [job: 'test-2', params: [string(name: 'city', value: 'jogja'), string(name: 'province', value: 'jogja')]],
-    [job: 'test-3', params: [string(name: 'city', value: 'bandung'), string(name: 'province', value: 'west java')]],
-    [job: 'test-4', params: [string(name: 'city', value: 'jakarta'), string(name: 'province', value: 'jakarta')]],
-    [job: 'test-5', params: [string(name: 'city', value: 'semarang'), string(name: 'province', value: 'central java')]],
-    [job: 'test-6', params: [string(name: 'city', value: 'surabaya'), string(name: 'province', value: 'east java')]],
-    [job: 'test-7', params: [string(name: 'city', value: 'medan'), string(name: 'province', value: 'east sumatera')]],
-    [job: 'test-8', params: [string(name: 'city', value: 'padang'), string(name: 'province', value: 'west sumatera')]],
-    [job: 'test-9', params: [string(name: 'city', value: 'aceh'), string(name: 'province', value: 'aceh darussalam')]],
-    [job: 'test-10', params: [string(name: 'city', value: 'riau'), string(name: 'province', value: 'riau')]]
+    [job: 'testing/test-1', params: [string(name: 'city', value: 'bogor'), string(name: 'province', value: 'west java')]],
+    [job: 'testing/test-2', params: [string(name: 'city', value: 'jogja'), string(name: 'province', value: 'jogja')]],
+    [job: 'testing/test-3', params: [string(name: 'city', value: 'bandung'), string(name: 'province', value: 'west java')]],
+    [job: 'testing/test-4', params: [string(name: 'city', value: 'jakarta'), string(name: 'province', value: 'jakarta')]],
+    [job: 'testing/test-5', params: [string(name: 'city', value: 'semarang'), string(name: 'province', value: 'central java')]],
+    [job: 'testing/test-6', params: [string(name: 'city', value: 'surabaya'), string(name: 'province', value: 'east java')]],
+    [job: 'testing/test-7', params: [string(name: 'city', value: 'medan'), string(name: 'province', value: 'east sumatera')]],
+    [job: 'testing/test-8', params: [string(name: 'city', value: 'padang'), string(name: 'province', value: 'west sumatera')]],
+    [job: 'testing/test-9', params: [string(name: 'city', value: 'aceh'), string(name: 'province', value: 'aceh darussalam')]],
+    [job: 'testing/test-10', params: [string(name: 'city', value: 'riau'), string(name: 'province', value: 'riau')]]
 ]
