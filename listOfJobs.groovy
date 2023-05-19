2e0943f6bbdbf4d42609e8f12bdbe1c0e9206359
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-19 16:32:47 +0700

    Update listOfJobs.groovy

--- listOfJobs.groovy
+++ listOfJobs.groovy
@@ -1,6 +1,3 @@
-import groovy.transform.Field
-
-@Field
 def jobsToTrigger = [
     [job: 'test-1', params: [string(name: 'city', value: 'bogor'), string(name: 'province', value: 'west java')]],
     [job: 'test-2', params: [string(name: 'city', value: 'jogja',), string(name: 'province', value: 'jogja')]],
@@ -10,5 +7,3 @@ def jobsToTrigger = [
     [job: 'test-6', params: [string(name: 'city', value: 'surabaya'), string(name: 'province', value: 'east java')]],
     [job: 'test-7', params: [string(name: 'city', value: 'medan'), string(name: 'province', value: 'east sumatera')]]
 ]
-
-return this;
