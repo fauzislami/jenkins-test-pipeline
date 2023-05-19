723c128ca65337557c212415b4ff36aee016475e
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-19 17:14:01 +0700

    Update listOfJobs.groovy

--- listOfJobs.groovy
+++ listOfJobs.groovy
@@ -6,5 +6,6 @@ jobsToTrigger = [
     [job: 'test-5', params: [string(name: 'city', value: 'semarang'), string(name: 'province', value: 'central java')]],
     [job: 'test-6', params: [string(name: 'city', value: 'surabaya'), string(name: 'province', value: 'east java')]],
     [job: 'test-7', params: [string(name: 'city', value: 'medan'), string(name: 'province', value: 'east sumatera')]],
-    [job: 'test-8', params: [string(name: 'city', value: 'padang'), string(name: 'province', value: 'west sumatera')]]
+    [job: 'test-8', params: [string(name: 'city', value: 'padang'), string(name: 'province', value: 'west sumatera')]],
+    [job: 'test-9', params: [string(name: 'city', value: 'aceh'), string(name: 'province', value: 'aceh darussalam')]]
 ]
