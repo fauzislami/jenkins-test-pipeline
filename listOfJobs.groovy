3f8d2ff5cf104285902205bd6c2937e2ee623463
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-05-19 18:33:43 +0700

    Update listOfJobs.groovy

--- listOfJobs.groovy
+++ listOfJobs.groovy
@@ -1,15 +1,15 @@
 jobsToTrigger = [
-    [job: 'testing/genap/test-2', params: [string(name: 'city', value: 'jogja'), string(name: 'province', value: 'jogja')]],
-    [job: 'testing/ganjil/test-3', params: [string(name: 'city', value: 'bandung'), string(name: 'province', value: 'west java')]],
-    [job: 'testing/genap/test-4', params: [string(name: 'city', value: 'jakarta'), string(name: 'province', value: 'jakarta')]],
-    [job: 'testing/ganjil/test-5', params: [string(name: 'city', value: 'semarang'), string(name: 'province', value: 'central java')]],
-    [job: 'testing/genap/test-6', params: [string(name: 'city', value: 'surabaya'), string(name: 'province', value: 'east java')]],
-    [job: 'testing/ganjil/test-7', params: [string(name: 'city', value: 'medan'), string(name: 'province', value: 'east sumatera')]],
-    [job: 'testing/genap/test-8', params: [string(name: 'city', value: 'padang'), string(name: 'province', value: 'west sumatera')]],
-    [job: 'testing/ganjil/test-9', params: [string(name: 'city', value: 'aceh'), string(name: 'province', value: 'aceh darussalam')]],
-    [job: 'testing/genap/test-10', params: [string(name: 'city', value: 'riau'), string(name: 'province', value: 'riau')]],
-    [job: 'testing/ganjil/test-11', params: [string(name: 'city', value: 'jambi'), string(name: 'province', value: 'jambi')]],
-    [job: 'testing/genap/test-12', params: [string(name: 'city', value: 'palembang'), string(name: 'province', value: 'south sumatera')]],
-    [job: 'testing/ganjil/test-13', params: [string(name: 'city', value: 'lampung'), string(name: 'province', value: 'bandar lampung')]]
+    [job: 'testing/even/test-2', params: [string(name: 'city', value: 'jogja'), string(name: 'province', value: 'jogja')]],
+    [job: 'testing/odd/test-3', params: [string(name: 'city', value: 'bandung'), string(name: 'province', value: 'west java')]],
+    [job: 'testing/even/test-4', params: [string(name: 'city', value: 'jakarta'), string(name: 'province', value: 'jakarta')]],
+    [job: 'testing/odd/test-5', params: [string(name: 'city', value: 'semarang'), string(name: 'province', value: 'central java')]],
+    [job: 'testing/even/test-6', params: [string(name: 'city', value: 'surabaya'), string(name: 'province', value: 'east java')]],
+    [job: 'testing/odd/test-7', params: [string(name: 'city', value: 'medan'), string(name: 'province', value: 'east sumatera')]],
+    [job: 'testing/even/test-8', params: [string(name: 'city', value: 'padang'), string(name: 'province', value: 'west sumatera')]],
+    [job: 'testing/odd/test-9', params: [string(name: 'city', value: 'aceh'), string(name: 'province', value: 'aceh darussalam')]],
+    [job: 'testing/even/test-10', params: [string(name: 'city', value: 'riau'), string(name: 'province', value: 'riau')]],
+    [job: 'testing/odd/test-11', params: [string(name: 'city', value: 'jambi'), string(name: 'province', value: 'jambi')]],
+    [job: 'testing/even/test-12', params: [string(name: 'city', value: 'palembang'), string(name: 'province', value: 'south sumatera')]],
+    [job: 'testing/odd/test-13', params: [string(name: 'city', value: 'lampung'), string(name: 'province', value: 'bandar lampung')]]
 ]
 
