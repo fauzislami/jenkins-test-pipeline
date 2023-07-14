884e42bd8dc68d11f6160dc7bbcd77bbbe393b9d
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-07-14 18:00:18 +0700

    Update listOfJobs.groovy

--- listOfJobs.groovy
+++ listOfJobs.groovy
@@ -1,5 +1,5 @@
 //Base Jobs for Win64 only
-UE4_27BaseJobs = [
+"UE4_27BaseJobs" = [
     [job: 'testing/UE4.27/DCL-Sample-Project-UE4.27-Win64-Shipping', params: [string(name: 'city', value: 'jogja'), string(name: 'province', value: 'jogja')]],
     [job: 'testing/UE4.27/DCL-Sample-Project-UE4.27-Win64-Debug', params: [string(name: 'city', value: 'bandung'), string(name: 'province', value: 'west java')]]
 ]
@@ -21,7 +21,7 @@ UE5_2BaseJobs = [
 
 
 //Jobs for other platforms
-UE4_27PlatformsJobs = [
+"UE4_27PlatformsJobs" = [
     [job: 'testing/UE4.27/DCL-Sample-Project-UE4.27-PS4-Shipping', params: [string(name: 'city', value: 'samarinda'), string(name: 'province', value: 'east borneo')]],
     [job: 'testing/UE4.27/DCL-Sample-Project-UE4.27-PS4-Debug', params: [string(name: 'city', value: 'pontianak'), string(name: 'province', value: 'west borneo')]]
 ]
