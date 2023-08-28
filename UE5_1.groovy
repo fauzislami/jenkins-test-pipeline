d0874b16e0ca6ef01b298e0c8b8078ae8190f905
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-08-28 14:54:53 +0700

    Update UE5_1.groovy

--- UE5_1.groovy
+++ UE5_1.groovy
@@ -1,4 +1,4 @@
-UE5_1BaseJobs = [
+baseJobs = [
 //    [job: 'testing/UE5.1/DCL-Sample-Project-UE5.1-Win64-Shipping', params: [string(name: 'city', value: 'serang'), string(name: 'province', value: 'banten')]],
 //    [job: 'testing/UE5.1/DCL-Sample-Project-UE5.1-Win64-Debug', params: [string(name: 'city', value: 'jakarta'), string(name: 'province', value: 'dki jakarta')]]
 ]
