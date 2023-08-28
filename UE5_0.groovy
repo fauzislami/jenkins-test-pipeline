5b1c777544af097ea48e0a823f43edb4c0e068bc
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-08-28 14:55:23 +0700

    Update UE5_0.groovy

--- UE5_0.groovy
+++ UE5_0.groovy
@@ -1,4 +1,4 @@
-baseJobs = [
+BaseJobs = [
     [job: 'testing/UE5.0/DCL-Sample-Project-UE5.0-Win64-Shipping', params: [string(name: 'city', value: 'semarang'), string(name: 'province', value: 'central java')]],
     [job: 'testing/UE5.0/DCL-Sample-Project-UE5.0-Win64-Debug', params: [string(name: 'city', value: 'surabaya'), string(name: 'province', value: 'east java')]]
 ]
