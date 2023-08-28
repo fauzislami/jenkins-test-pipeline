9478113d1b27dbdc344235e8a7b14a4871a8d0c3
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-08-28 14:35:48 +0700

    Create UE5_1.groovy

---
+++ UE5_1.groovy
@@ -0,0 +1,10 @@
+UE5_1BaseJobs = [
+//    [job: 'testing/UE5.1/DCL-Sample-Project-UE5.1-Win64-Shipping', params: [string(name: 'city', value: 'serang'), string(name: 'province', value: 'banten')]],
+//    [job: 'testing/UE5.1/DCL-Sample-Project-UE5.1-Win64-Debug', params: [string(name: 'city', value: 'jakarta'), string(name: 'province', value: 'dki jakarta')]]
+]
+
+UE5_1PlatformsJobs = [
+ //   [job: 'testing/UE5.1/DCL-Sample-Project-UE5.1-PS4-Shipping', params: [string(name: 'city', value: 'gorontalo'), string(name: 'province', value: 'gorontalo')]],
+ //   [job: 'testing/UE5.1/DCL-Sample-Project-UE5.1-PS4-Debug', params: [string(name: 'city', value: 'palu'), string(name: 'province', value: 'west sulawesi')]]
+]
+
