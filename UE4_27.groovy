5a709f7af1c5448dfe21516cfb6496cffaae1c5a
Muhammad Fauzi Islami <islamifauzi@gmail.com>
2023-08-29 23:28:32 +0700

    Update UE4_27.groovy

--- UE4_27.groovy
+++ UE4_27.groovy
@@ -1,9 +1,9 @@
-BaseJobs = [
+def BaseJobs = [
     [job: 'testing/UE4.27/DCL-Sample-Project-UE4.27-Win64-Shipping', params: [string(name: 'city', value: 'jogja'), string(name: 'province', value: 'jogja')]],
     [job: 'testing/UE4.27/DCL-Sample-Project-UE4.27-Win64-Debug', params: [string(name: 'city', value: 'bandung'), string(name: 'province', value: 'west java')]]
 ]
 
-PlatformsJobs = [
+def PlatformsJobs = [
     [job: 'testing/UE4.27/DCL-Sample-Project-UE4.27-PS4-Shipping', params: [string(name: 'city', value: 'samarinda'), string(name: 'province', value: 'east borneo')]],
     [job: 'testing/UE4.27/DCL-Sample-Project-UE4.27-PS4-Debug', params: [string(name: 'city', value: 'pontianak'), string(name: 'province', value: 'west borneo')]]
 ]
